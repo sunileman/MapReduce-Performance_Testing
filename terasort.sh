d3b911c85b130d5d11a187b0d2177287ef2db368
Sunile Manjee <sunilemanjeegmail.com>
2016-07-08 15:24:09 -0500

    ini

---
+++ terasort.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+trap "" HUP
+
+#if [ $EUID -eq 0 ]; then
+#   echo "this script must not be run as root. su to hdfs user to run"
+#   exit 1
+#fi
+
+MR_EXAMPLES_JAR=/usr/hdp/current/hadoop-mapreduce-client/hadoop-mapreduce-examples.jar
+
+
+#SIZE=500G
+#SIZE=100G
+SIZE=1T
+#SIZE=1G
+#SIZE=10G
+#INPUT=/${SIZE}-terasort-input
+#OUTPUT=/${SIZE}-terasort-output
+
+INPUT=/data/sandbox/poc/teragen/${SIZE}-terasort-input
+OUTPUT=/data/sandbox/poc/teragen/${SIZE}-terasort-output
+
+# terasort.sh
+# Kill any running MapReduce jobs
+mapred job -list | grep job_ | awk ' { system("mapred job -kill " $1) } '
+# Delete the output directory
+hadoop fs -rm -r -f -skipTrash ${OUTPUT}
+
+# Run terasort
+time hadoop jar $MR_EXAMPLES_JAR terasort \
+-Dmapreduce.map.log.level=INFO \
+-Dmapreduce.reduce.log.level=INFO \
+-Dyarn.app.mapreduce.am.log.level=INFO \
+-Dio.file.buffer.size=131072 \
+-Dmapreduce.map.cpu.vcores=1 \
+-Dmapreduce.map.java.opts=-Xmx1536m \
+-Dmapreduce.map.maxattempts=1 \
+-Dmapreduce.map.memory.mb=2048 \
+-Dmapreduce.map.output.compress=true \
+-Dmapreduce.map.output.compress.codec=org.apache.hadoop.io.compress.Lz4Codec \
+-Dmapreduce.reduce.cpu.vcores=1 \
+-Dmapreduce.reduce.java.opts=-Xmx1536m \
+-Dmapreduce.reduce.maxattempts=1 \
+-Dmapreduce.reduce.memory.mb=2048 \
+-Dmapreduce.task.io.sort.factor=300 \
+-Dmapreduce.task.io.sort.mb=384 \
+-Dyarn.app.mapreduce.am.command.opts=-Xmx768m \
+-Dyarn.app.mapreduce.am.resource.mb=1024 \
+-Dmapred.reduce.tasks=92 \
+-Dmapreduce.terasort.output.replication=1 \
+${INPUT} ${OUTPUT}
---
+++ validate.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+trap "" HUP
+
+#if [ $EUID -eq 0 ]; then
+#   echo "this script must not be run as root. su to hdfs user to run"
+#   exit 1
+#fi
+
+#MR_EXAMPLES_JAR=/usr/hdp/2.3.2.0-2950/hadoop-mapreduce/hadoop-mapreduce-examples.jar
+MR_EXAMPLES_JAR=/usr/hdp/current/hadoop-mapreduce-client/hadoop-mapreduce-examples.jar
+
+#SIZE=500G
+#SIZE=100G
+SIZE=1T
+#SIZE=1G
+#SIZE=10G
+OUTPUT=/data/sandbox/poc/teragen/${SIZE}-terasort-output
+REPORT=/data/sandbox/poc/teragen/${SIZE}-terasort-report
+
+
+# teravalidate.sh
+# Kill any running MapReduce jobs
+mapred job -list | grep job_ | awk ' { system("mapred job -kill " $1) } '
+# Delete the output directory
+hadoop fs -rm -r -f -skipTrash ${REPORT}
+# Run teravalidate
+time hadoop jar $MR_EXAMPLES_JAR teravalidate \
+-Ddfs.blocksize=256M \
+-Dio.file.buffer.size=131072 \
+-Dmapreduce.map.memory.mb=2048 \
+-Dmapreduce.map.java.opts=-Xmx1536m \
+-Dmapreduce.reduce.memory.mb=2048 \
+-Dmapreduce.reduce.java.opts=-Xmx1536m \
+-Dyarn.app.mapreduce.am.resource.mb=1024 \
+-Dyarn.app.mapreduce.am.command-opts=-Xmx768m \
+-Dmapreduce.task.io.sort.mb=1 \
+-Dmapred.map.tasks=185 \
+-Dmapred.reduce.tasks=185 \
+${OUTPUT} ${REPORT}
